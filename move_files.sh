613a6e98786f4408fc9cde522026a97bef027dd6
Siyuan Jiang <2203934+sjiang1@users.noreply.github.com>
2020-09-28 05:57:29 -0400

    simplified move_files.sh

--- move_files.sh
+++ move_files.sh
@@ -1,18 +1,9 @@
 #! /usr/bin/bash
-if [ -e demo ]; then
-    if [ ! -e demo/cosc_381 ]; then
-        mkdir demo/cosc_381
-    fi
-fi
+mkdir -p demo/cosc_381
 
 for file in $(ls demo/cosc_381_*)
 do
-    if [ ! -f $file ]; then
-        # skip
-        echo "not a file"
-    else
-        file_name=$(basename $file)
-        echo "moving $file_name"
-        mv $file demo/cosc_381/$file_name
-    fi
+    file_name=$(basename $file)
+    echo "moving $file_name"
+    mv $file demo/cosc_381/$file_name
 done
